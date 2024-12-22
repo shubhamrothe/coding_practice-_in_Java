2657b068ca7c59c93767df3bd45a9c9accdf27b5
shubhamrothe <shubhamrothe01@gmail.com>
2024-12-22 10:54:25 +0530

    do some modification as per coding standard in Que02

--- 75_Questions_Coding_Challange/DSAQuestionsSeries/src/com/example/ShuffleTheArray.java
+++ 75_Questions_Coding_Challange/DSAQuestionsSeries/src/com/example/ShuffleTheArray.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.Scanner;
 
 /*
- * Shuffle the Array
+Shuffle the Array
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 */
